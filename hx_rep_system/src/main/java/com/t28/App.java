4ee2f22cdf31cae88dd33ac2ef80325835eeec0a
Forest <forest1xiao@icloud.com>
2019-10-10 12:24:14 +0800

    commit Test

--- hx_rep_system/src/main/java/com/t28/App.java
+++ hx_rep_system/src/main/java/com/t28/App.java
@@ -1,5 +1,6 @@
 package com.t28;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -11,6 +12,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
