068a29203983a3181a794786e4a86df02eef930b
zsy <1835263553@qq.com>
2019-10-16 16:49:17 +0800

    零售DAO接口(未完成)

---
+++ hx_rep_system/src/main/java/com/t28/forest/retail/dao/DepotitemDAO.java
@@ -0,0 +1,11 @@
+package com.t28.forest.retail.dao;
+
+/**
+ * 功能描述:DepotitemDAO接口
+ *
+ * @since: 1.0.0
+ * @Author: Zsy
+ * @Date: 2019/10/16$
+ */
+public interface DepotitemDAO {
+}
---
+++ hx_rep_system/src/main/java/com/t28/forest/retail/dao/MaterialDAO.java
@@ -0,0 +1,11 @@
+package com.t28.forest.retail.dao;
+
+/**
+ * 功能描述:MaterialDAO接口
+ *
+ * @since: 1.0.0
+ * @Author: Zsy
+ * @Date: 2019/10/16$
+ */
+public interface MaterialDAO {
+}
