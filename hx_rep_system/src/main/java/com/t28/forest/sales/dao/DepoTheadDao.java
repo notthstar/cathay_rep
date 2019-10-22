39a16134aab17181b3522a810066d0ba9709bd31
Forest <forest1xiao@icloud.com>
2019-10-22 14:51:20 +0800

    分页类完善

--- hx_rep_system/src/main/java/com/t28/forest/sales/dao/DepoTheadDao.java
+++ hx_rep_system/src/main/java/com/t28/forest/sales/dao/DepoTheadDao.java
@@ -4,6 +4,7 @@ import com.t28.forest.sales.vo.PageVO;
 import com.t28.forest.sales.vo.SalesBillVO;
 import com.t28.forest.shared.cond.Condition;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
  * @create 2019/10/16
  * @since 1.0.0
  */
+@Repository
 public interface DepoTheadDao {
 
     /**
