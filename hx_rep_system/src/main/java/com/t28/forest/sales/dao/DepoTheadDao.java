0e7ddb478b4fe70169cce89f2cc255359a4240f1
Forest <forest1xiao@icloud.com>
2019-10-22 09:14:57 +0800

    销售管理模块实体类已经删除

--- hx_rep_system/src/main/java/com/t28/forest/sales/dao/DepoTheadDao.java
+++ hx_rep_system/src/main/java/com/t28/forest/sales/dao/DepoTheadDao.java
@@ -1,24 +1,24 @@
 package com.t28.forest.sales.dao;
 
-import com.t28.forest.sales.entity.vo.PageVO;
-import com.t28.forest.sales.entity.vo.SalesBillVO;
+import com.t28.forest.sales.vo.PageVO;
+import com.t28.forest.sales.vo.SalesBillVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 /**
  * @author XiangYuFeng
- * @description 财务主表DAO层接口
+ * @description 单据主表DAO层接口
  * @create 2019/10/16
  * @since 1.0.0
  */
-public interface AccounTheadDao {
+public interface DepoTheadDao {
 
     /**
      * 分页查询单据信息
      * @param pageVO
      * @return List<SalesBillVO>
      */
-    public List<SalesBillVO> findAccountByPage(@Param("pageVO") PageVO pageVO);
+    public List<SalesBillVO> findBillByPage(@Param("pageVO") PageVO pageVO);
 
 }
--- hx_rep_system/src/main/java/com/t28/forest/user/dao/UserDao.java
+++ hx_rep_system/src/main/java/com/t28/forest/user/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.t28.forest.user.dao;
 
+import com.t28.forest.user.entity.UserDTO;
+
 /**
  * @author XiangYuFeng
  * @description 用户DAO层接口
@@ -7,4 +9,11 @@ package com.t28.forest.user.dao;
  * @since 1.0.0
  */
 public interface UserDao {
+
+    /**
+     * 通过条件查询用户
+     * @return UserDTO
+     */
+    public UserDTO findUserByCondition(UserDTO userDTO);
+
 }
